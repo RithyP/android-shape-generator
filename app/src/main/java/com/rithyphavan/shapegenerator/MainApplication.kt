2f5f59eee5eea9650ecb2585a0c694cc163027b4
RithyPhavan <phavanrithy@gmail.com>
2023-08-07 20:49:30 +0700

    Initial commit

---
+++ app/src/main/java/com/rithyphavan/shapegenerator/MainApplication.kt
@@ -0,0 +1,6 @@
+package com.rithyphavan.shapegenerator
+
+import android.app.Application
+
+class MainApplication : Application() {
+}
---
+++ app/src/main/java/com/rithyphavan/shapegenerator/Shape.kt
@@ -0,0 +1,6 @@
+package com.rithyphavan.shapegenerator
+
+abstract class Shape {
+
+    abstract fun draw()
+}
