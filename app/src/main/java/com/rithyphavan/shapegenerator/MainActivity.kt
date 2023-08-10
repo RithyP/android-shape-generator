185a3f60fae82b6b632a6a3d4f83b9f6e3cb9558
RithyPhavan <phavanrithy@gmail.com>
2023-08-10 12:28:59 +0700

    update the ui and add a function to generate the shapes

--- app/src/main/java/com/rithyphavan/shapegenerator/MainActivity.kt
+++ app/src/main/java/com/rithyphavan/shapegenerator/MainActivity.kt
@@ -1,48 +1,49 @@
 package com.rithyphavan.shapegenerator
 
-import android.widget.ArrayAdapter
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Rect
 import androidx.activity.viewModels
 import com.rithyphavan.shapegenerator.databinding.ActivityMainBinding
-import com.rithyphavan.shapegenerator.ui.component.shape.ShapeType
-import com.rithyphavan.shapegenerator.ui.component.shape.ShapeType.Companion.shapeByName
-import com.rithyphavan.shapegenerator.ui.component.shape.Square
+import com.rithyphavan.shapegenerator.ui.component.shape.Rectangle
+import com.rithyphavan.shapegenerator.util.ColorUtil.genRandomPaintColor
 
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
 
     private val viewModel: MainViewModel by viewModels()
 
-    private val shapeSelectorAdapter by lazy {
-        ArrayAdapter(
-            this,
-            R.layout.item_menu_drop_down,
-            ShapeType.values().map {
-                getString(it.shapeName)
-            },
-        )
-    }
-
     override fun initView() {
-        binding.shapeSelectorList.setAdapter(shapeSelectorAdapter)
     }
 
     override fun initAction() {
-        binding.shapeSelectorList.setOnItemClickListener { parent, view, position, id ->
-            onShapeSelected(binding.shapeSelectorList.text.toString())
+
+        binding.generateShapeBtn.setOnClickListener {
+            drawShapeOnBitmap()
         }
     }
 
-    private fun onShapeSelected(selectedShape: String) {
-        viewModel.setSelectedShape(selectedShape.shapeByName())
-        when (selectedShape.shapeByName()) {
-
-            ShapeType.SQUARE -> {
-
+    private fun drawShapeOnBitmap() {
+        val shapeBitmap = Bitmap.createBitmap(
+            binding.shapeCanvasCard.width, binding.shapeCanvasCard.height, Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(shapeBitmap)
+        var shapeRect: Rect? = null
+        for (shape in viewModel.shapes) {
+            if (shape is Rectangle) {
+                shape
+            } else {
+                shape.setSize(200f)
             }
-
-            else -> {
-
-            }
+            shape.setShapePaint(genRandomPaintColor())
+            val bitmap = shape.draw()
+            shapeRect = Rect(
+                shapeRect?.right ?: 0, 0, bitmap.width + (shapeRect?.right ?: 0), bitmap.height
+            )
+            canvas.drawBitmap(
+                bitmap, null, shapeRect, null
+            )
         }
+        binding.shapeImage.setImageBitmap(shapeBitmap)
     }
 
 }
--- app/src/main/java/com/rithyphavan/shapegenerator/MainViewModel.kt
+++ app/src/main/java/com/rithyphavan/shapegenerator/MainViewModel.kt
@@ -2,12 +2,23 @@ package com.rithyphavan.shapegenerator
 
 import android.graphics.Bitmap
 import androidx.lifecycle.ViewModel
+import com.rithyphavan.shapegenerator.ui.component.shape.Circle
+import com.rithyphavan.shapegenerator.ui.component.shape.Rectangle
+import com.rithyphavan.shapegenerator.ui.component.shape.Shape
 import com.rithyphavan.shapegenerator.ui.component.shape.ShapeType
+import com.rithyphavan.shapegenerator.ui.component.shape.Square
+import com.rithyphavan.shapegenerator.ui.component.shape.Triangle
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class MainViewModel : ViewModel() {
+
+    var shapes: List<Shape> = listOf(
+        Square(),
+        Circle(),
+        Rectangle(),
+        Triangle(),
+    )
 
     private var selectedShape: ShapeType? = null
     fun setSelectedShape(selectedShape: ShapeType?) {
